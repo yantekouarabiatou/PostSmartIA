92164672f888f3fb9f9da29ff05bafa30d47a1fb
yantekouarabiatou <yantekouarabiatou@gmail.com>
2026-05-06 20:04:51 +0100

    Ajout des fonctionnalités

--- postaSmartAIbackend/bootstrap/app.php
+++ postaSmartAIbackend/bootstrap/app.php
@@ -15,9 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Enable CORS for API
-        $middleware->statefulApi();
-
         $middleware->web(append: [
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
